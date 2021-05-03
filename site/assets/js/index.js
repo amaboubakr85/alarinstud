6f6a35715a95cbfe54bfc7e8cf7c2a9c945eb770
amaboubakr85 <a.m.aboubakr85@gmail.com>
2021-05-03 15:03:28 +0300

    deploy single horse

--- site/assets/js/index.js
+++ site/assets/js/index.js
@@ -1,1 +1,36 @@
-console.log('success')
+const planningGallery = document.querySelector('.planning-gallery')
+
+if (planningGallery) {
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  })
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  })
+}
+
+let play = document.getElementById('play')
+if (play) {
+  play.addEventListener('click', function () {
+    let play_video = document.querySelector('.video-play')
+    document.querySelector('.overlay').style.opacity = '0'
+    document.querySelector('.overlay').style.zIndex = '0'
+    play_video.play()
+    play_video.controls = true
+  })
+}
